f63839820bfcc513ec43f83336d33812b2514ef3
wingo <wingoastana@gmail.com>
2025-08-28 15:56:31 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='wingo-v19';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','styles.css','app.js','menu.json','config.json'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})).catch(()=>caches.match('index.html')));});
